bdb4558e3c17b4d48f1313d7eb346ac700e446c4
Peter Czanik <peter.czanik@balabit.com>
2017-07-20 17:37:35 +0200

    Update version numbers, replace place holders
    - updated version numbers to 3.10.1
    - replaced help.1 with minimal documentation
      and pointers
    - added GPL and LGPL to licenses
    - added rhel-7-server-optional-rpm to repositories
    - removed "java" dependency at least for now
    
    Signed-off-by: Peter Czanik <peter.czanik@balabit.com>

--- run-container.sh
+++ run-container.sh
@@ -1,1 +1,1 @@
-docker run -it -v /data/syslog-ng/conf/syslog-ng.conf:/etc/syslog-ng/syslog-ng.conf -v /data/syslog-ng/logs:/var/log -p 514:514 -p 601:601 --name glen-syslog rhel73/syslog-ng:3.5.6-3 -edv
+docker run -it -v /data/syslog-ng/conf/syslog-ng.conf:/etc/syslog-ng/syslog-ng.conf -v /data/syslog-ng/logs:/var/log -p 514:514 -p 601:601 --name container-syslog rhel73/syslog-ng:3.10.1-1 -edv
